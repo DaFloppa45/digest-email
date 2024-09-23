4e62f661af00ff06c085a02540b15267163fb347
DaFloppa45 <m.latinomain@icloud.com>
2024-09-24 00:02:02 +0100

    added calendar retrieval

--- de_content.py
+++ de_content.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 # --- Weather Forecast Config ---
 from pyowm.owm import OWM
 from pyowm.utils import formatting, measurables
@@ -5,6 +6,22 @@ from pyowm.utils import formatting, measurables
 owm = OWM('c92d7336677e626ce0c6d9eca0be3777') # OWM API Key
 mgr = owm.weather_manager()
 # -------------------------------
+
+# --- ICloud Config ---
+# app-specific password: reim-wvbd-kynv-dsqj
+import caldav
+from caldav.elements import dav, cdav
+
+num = 52
+dsid = 21409152914
+guid = 'E6E94AC8-9F66-40EC-A96B-CD9618604AB4'
+
+icloud_client = caldav.DAVClient(
+    url='https://caldav.icloud.com/',
+    username='m.latinomain@icloud.com',
+    password='reim-wvbd-kynv-dsqj'
+)
+# ---------------------
 
 def get_forecast():
     # Initial Setup
@@ -23,18 +40,36 @@ def get_forecast():
         entry['temp'] = measurables.kelvin_dict_to(per['temperature'],'celsius')['temp']
         entry['weather_icon'] = per['weather_icon_name']
         forecast['periods'].append(entry)
-        
+
     return forecast
         
-        
-    
-
 def get_events():
-    pass
+    principal = icloud_client.principal()
+    calendars = principal.calendars()
+    output = []
+    for calendar in calendars:
+        events = calendar.date_search(
+            start=datetime.now(),
+            end=datetime.now() + timedelta(days=1)
+        )
+        for event in events:
+            name = event.vobject_instance.vevent.summary.value
+            start_time = event.vobject_instance.vevent.dtstart.value
+            end_time = event.vobject_instance.vevent.dtend.value
+            output.append({
+                'name': name,
+                'start_time': start_time.strftime("%H:%M:%S"),
+                'end_time': end_time.strftime("%H:%M:%S")
+                })
+        
+    return output
+
 
 def get_finance():
     pass
 
 if __name__ == "__main__":
-    print(get_forecast())
+    print(get_events())
+
     
+        
